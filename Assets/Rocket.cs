69235e6f5e9b1759229b2ce692df6895552588e1
Peter Böös <peterdotboos@gmail.com>
2018-01-13 18:29:59 +0100

    Rocket added
    Rocket prefab v1
    Input stub WIP

---
+++ Assets/Rocket.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class Rocket : MonoBehaviour {
+
+        // Use this for initialization
+        void Start ()
+        {
+		
+        }
+	
+        // Update is called once per frame
+        void Update ()
+        {
+            ProcessInput();
+        }
+
+        private void ProcessInput()
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                print("Space pressed");
+            }
+
+            if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
+            {
+                // Do nothing
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                print("Left");
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                print("Right");
+            }
+        }
+    }
+}
